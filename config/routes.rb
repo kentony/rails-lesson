1ba9c5e8d081f47e5612ef8fffa01fab492f2b8b
Kentony <kenken.nyanko.1115@gmail.com>
2019-06-14 06:58:39 +0000

    lesson1-3 copy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
-  get '/about' => "home#about"
-
+  
   get 'posts/index' => "posts#index"
+  get 'posts/:id' => "posts#show"
+
+  
+  get '' => "home#top"
+  get 'about' => "home#about"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
